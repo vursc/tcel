9bdc2f73d5a20eb5ee35c93c2b7db72c914f414d
Vursc <gz_wangxm@outlook.com>
2022-01-22 19:44:19 +0800

    improve(show): Print last named subsect before one unnamed in command 'show'

--- command-show.c
+++ command-show.c
@@ -105,15 +105,34 @@ enum err cmd_show_subsect(char *filename, unsigned int si, unsigned int ssi, boo
 
 	struct sect *s = find_sect(rec, si);
 	if (s == NULL) goto err_free_rec;
-	struct subsect *ss = find_subsect(s->head, ssi);
-	if (ss == NULL) goto err_free_rec;
+
+	struct subsect *ss = s->head;
+	struct subsect *last_named_ss = NULL;
+	unsigned int last_named_ssi = 0;
+	// If the ssi-th subsect is the first named subsect,
+	// last_named_ss does not necessarily point to it,
+	// since it will not be accessed
+	for (unsigned int i = 0; i < ssi; ++i) {
+		if (ss == NULL) goto err_out_of_range;
+		if (ss->name != NULL) {
+			last_named_ss = ss;
+			last_named_ssi = i;
+		}
+		ss = ss->next;
+	}
+
+	if (ss == NULL) goto err_out_of_range;
 	show_sect_header(s->head->name, si, color);
+	if (ss->name == NULL && last_named_ss != NULL)
+		show_subsect_header(last_named_ss->name, si, last_named_ssi, color);
 	show_subsect(ss, si, ssi, color);
 	putchar('\n');
 
 	free_record(rec);
 	return ERR_OK;
 
+err_out_of_range:
+	fprintf(stderr, "Subsection index out of range\n");
 err_free_rec:
 	free_record(rec);
 	return ERR_ERR;
